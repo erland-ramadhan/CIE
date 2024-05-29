e1149e85436760c8533c2e93593fcd968a4717cd
erland-ramadhan <48809651+erland-ramadhan@users.noreply.github.com>
2024-05-29 10:50:55 +0700

    Input for Revision Accepted

--- cryptography.py
+++ cryptography.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 def circle_map(x, omega, K):
     """Implements the circle map function."""
@@ -12,22 +13,12 @@ def composed_map(x, alpha, beta, omega, K):
     """Implements the composed map function."""
     return gauss_map(circle_map(x, omega, K), alpha, beta)
 
-def int_to_bin_array(num):
-    """Converts an integer to a binary array."""
-    return np.array(list(np.binary_repr(num, width=8))).astype(np.uint8)
-
-def bin_array_to_int(bin_array):
-    """Converts a binary array to an integer."""
-    return int("".join(bin_array.astype(str)), 2)
-
 def calculate_rmse(image1, image2):
     """Calculates the Root Mean Square Error (RMSE) between two images."""
-    # return np.sqrt(np.mean((image1 - image2) ** 2))
     return np.sqrt(np.mean(np.subtract(image1, image2) ** 2))
 
 def calculate_psnr(image1, image2):
     """Calculates the Peak Signal-to-Noise Ratio (PSNR) between two images."""
-    # mse = np.mean((image1 - image2) ** 2)
     mse = np.mean(np.subtract(image1, image2) ** 2)
     if mse == 0:
         return float('inf')
@@ -48,12 +39,10 @@ def generate_keystream(func, size, *args):
         elif func == 'gauss-map':
             x = gauss_map(x, alpha, beta)
         else:
-        # elif func == 'circle-gauss-com':
             x = composed_map(x, alpha, beta, omega, K)
 
-        keystream.append(int(x * 1e6) % 256)
+        keystream.append(math.floor(x * 1e6) % 256)
 
-    # return np.array(keystream, dtype=np.uint8).reshape(size)
     return np.array(keystream, dtype=np.uint8)
 
 def encrypt(image_matrix, func, *args):
@@ -74,16 +63,6 @@ def encrypt(image_matrix, func, *args):
             result = np.bitwise_xor(result_, encrypted_flat[-1])
             encrypted_flat.append(result)
             i += 1
-
-        # encrypted_mat = np.zeros_like(image_matrix)
-
-        # for i in range(size[0]):
-        #     for j in range(size[1]):
-        #         for k in range(size[2]):
-        #             image_mat_bin = int_to_bin_array(image_matrix[i, j, k])
-        #             keystream_bin = int_to_bin_array(keystream[i, j, k])
-        #             encrypted_bin = np.bitwise_xor(image_mat_bin, keystream_bin)
-        #             encrypted_mat[i, j, k] = bin_array_to_int(encrypted_bin)
 
         encrypted_flat = np.array(encrypted_flat)
         encrypted_mat = encrypted_flat.reshape(size)
@@ -108,15 +87,7 @@ def decrypt(image_matrix, func, *args):
             decrypted_flat.append(result)
             i += 1
 
-        # for i in range(size[0]):
-        #     for j in range(size[1]):
-        #         for k in range(size[2]):
-        #             image_mat_bin = int_to_bin_array(image_matrix[i, j, k])
-        #             keystream_bin = int_to_bin_array(keystream[i, j, k])
-        #             decrypted_bin = np.bitwise_xor(image_mat_bin, keystream_bin)
-        #             decrypted_mat[i, j, k] = bin_array_to_int(decrypted_bin)
-
         decrypted_flat = np.array(decrypted_flat)
-        decrypted_mat = decrypted_map.reshape(size)
+        decrypted_mat = decrypted_flat.reshape(size)
 
         return decrypted_mat
